6f1b81a02fee77e95fb439483db60e5195c92d2f
emericdefay <emericdefay@gmail.com>
2023-01-05 16:54:41 +0100

    com

--- SQLITE-CALLBACK.cob
+++ SQLITE-CALLBACK.cob
@@ -3,15 +3,15 @@
        DATA DIVISION. 
        WORKING-STORAGE SECTION.
        LINKAGE SECTION.
-       01 COLUMN-ID         PIC 9(4).
-       01 COLUMN-NAME       PIC 9(4).
+       01 COLUMN-ID         PIC 99.
+       01 COLUMN-NAME       PIC X(20).
        01 SUM-RESULT        PIC 9(4) GLOBAL.
        01 QUERY             PIC X(80).
        01 ARGC              PIC 99 COMP-5.
        01 NOTUSED           POINTER.
        01 ARGV.
            03  FIRSTCOLUMN  POINTER.
-           03  SECONDCOLUMN  POINTER.
+           03  SECONDCOLUMN POINTER.
        01 AZCOLNAME         POINTER.
 
        PROCEDURE DIVISION USING
@@ -22,10 +22,11 @@
 
            SET ADDRESS OF COLUMN-ID TO FIRSTCOLUMN
            SET ADDRESS OF COLUMN-NAME TO SECONDCOLUMN
-           DISPLAY "SQL > COLUMN-ID : " COLUMN-NAME
+           DISPLAY "SQL > COLUMN-ID : " COLUMN-ID
+           DISPLAY "SQL > COLUMN-NAME : " COLUMN-NAME
 
       *        Call sum function
-           CALL "MAIN" USING BY REFERENCE COLUMN-NAME, SUM-RESULT
+           CALL "MAIN" USING BY REFERENCE COLUMN-ID, SUM-RESULT
 
            GOBACK.
        END PROGRAM SQLITE-CALLBACK.
