b06fe9044ffbbdcd5f6d9c032b51137ea86bb2af
emericdefay <emericdefay@gmail.com>
2023-01-05 16:56:45 +0100

    com

--- MAIN.cob
+++ MAIN.cob
@@ -4,13 +4,13 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        LINKAGE SECTION.
-       01 COLUMN-NAME         PIC 999.
+       01 COLUMN-ID         PIC 99.
        01 SUM-RESULT        PIC 9(4) GLOBAL.
 
-       PROCEDURE DIVISION USING BY REFERENCE COLUMN-NAME, 
+       PROCEDURE DIVISION USING BY REFERENCE COLUMN-ID, 
                                              SUM-RESULT.
       * Calculate sum of array elements
-           DISPLAY "MAIN > COLUMN-ID : " COLUMN-NAME
-           ADD COLUMN-NAME TO SUM-RESULT
+           DISPLAY "MAIN > COLUMN-ID : " COLUMN-ID
+           ADD COLUMN-ID TO SUM-RESULT
 
            EXIT PROGRAM.
