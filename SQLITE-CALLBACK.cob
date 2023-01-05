36eceede7100fd4fbbc0cc469820a05c2905df5e
emericdefay <emericdefay@gmail.com>
2023-01-05 16:52:31 +0100

    com

--- SQLITE-CALLBACK.cob
+++ SQLITE-CALLBACK.cob
@@ -3,10 +3,9 @@
        DATA DIVISION. 
        WORKING-STORAGE SECTION.
        LINKAGE SECTION.
-       01 COLUMN-ID         PIC 999.
-       01 COLUMN-NAME         PIC 999.
+       01 COLUMN-ID         PIC 9(4).
+       01 COLUMN-NAME       PIC 9(4).
        01 SUM-RESULT        PIC 9(4) GLOBAL.
-       01 COLUMN-NAME       PIC X(20).
        01 QUERY             PIC X(80).
        01 ARGC              PIC 99 COMP-5.
        01 NOTUSED           POINTER.
