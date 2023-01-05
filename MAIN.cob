cb7a47dcf88c021844e23768689f4ba676678cd3
emericdefay <emericdefay@gmail.com>
2023-01-05 15:05:30 +0100

    com

--- MAIN.cob
+++ MAIN.cob
@@ -4,15 +4,15 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        LINKAGE SECTION.
-       01  NUM    PIC 9(4).
+       01  RC     PIC 9 COMP-5.
        01  RESULT PIC 9(4).
 
        PROCEDURE DIVISION USING 
-                                NUM
+                                RC
                           RETURNING 
                                 RESULT.
       * Calculate sum of array elements
-           DISPLAY NUM
-           ADD NUM TO RESULT
+           DISPLAY "RC " RC
+           ADD RC TO RESULT
 
            EXIT PROGRAM.
--- TEST-SUM.cob
+++ TEST-SUM.cob
@@ -54,7 +54,7 @@
                BY VALUE     0
                BY REFERENCE ERR
                RETURNING RC
-               DISPLAY RC
+               DISPLAY "RC " RC
       *        Call sum function
                CALL "MAIN" USING BY REFERENCE RC RETURNING RESULT
            END-CALL
