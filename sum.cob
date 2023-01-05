6db3fb03a1cabf73ce76d346a985f6a6a60e8daa
emericdefay <emericdefay@gmail.com>
2023-01-05 08:50:01 +0100

    com

--- sum.cob
+++ sum.cob
@@ -7,7 +7,9 @@
        01  NUM    PIC 9(4) VALUE 0.
        01  RESULT PIC 9(4) VALUE 0.
 
-       PROCEDURE DIVISION USING NUM,
+       PROCEDURE DIVISION USING 
+                                NUM
+                          GIVING 
                                 RESULT.
       * Calculate sum of array elements
            ADD NUM TO RESULT
