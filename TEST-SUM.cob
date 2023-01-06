37792ad0bbeb64115c822b178f1a7d9d3c02086d
emericdefay <emericdefay@gmail.com>
2023-01-06 10:39:26 +0100

    com

--- TEST-SUM.cob
+++ TEST-SUM.cob
@@ -14,17 +14,18 @@
        01 RC                 PIC 9 COMP-5.
        01 CALLBACK           PROCEDURE-POINTER.
 
-      * RESULTS
 
        LINKAGE SECTION.
+      * RESULTS
        01 SUM-RESULT        PIC 9(4) VALUE 0.
+      * SQL CALLBACK
        01 ARGC              PIC 99 COMP-5.
        01 NOTUSED           POINTER.
        01 ARGV.
            03  FIRSTCOLUMN  POINTER.
            03  SECONDCOLUMN POINTER.
        01 AZCOLNAME         POINTER.
-           
+      * SQL CALLBACK SELECT
        01 COLUMN-ID         PIC 999.
        01 COLUMN-NAME       PIC X(20).
 
