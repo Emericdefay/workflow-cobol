60e393e66c6ab3b886cdaacde53bce8f9ce9b4d1
emericdefay <emericdefay@gmail.com>
2023-01-06 11:23:32 +0100

    com

--- MAIN.cob
+++ MAIN.cob
@@ -4,7 +4,7 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 TEST-A PIC 999.
+       01 INCREMENT PIC 9(5).
        LINKAGE SECTION.
        01 SUM-RESULT        PIC 9(5).
        01 COLUMN-ID         PIC X(3).
@@ -14,6 +14,7 @@
       * Calculate sum of array elements
            DISPLAY "MAIN > SUM-RESULT : " SUM-RESULT
            DISPLAY "MAIN > COLUMN-ID : "  COLUMN-ID
-      *    ADD COLUMN-ID TO SUM-RESULT
 
+           MOVE COLUMN-ID TO INCREMENT.
+           ADD INCREMENT TO SUM-RESULT
            EXIT PROGRAM.
